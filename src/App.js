37c5a539bc88b0f905af0da37e2adfef5cee8bb6
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-12 10:59:15 +0600

    axios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Button } from '@material-ui/core';
 
 import './App.css';
 import News from './components/News/News';
+import axios from 'axios';
 
 function App() {
   const [articles,setArticles]=useState([]);
@@ -10,7 +11,12 @@ function App() {
     const url='https://newsapi.org/v2/top-headlines?country=us&apiKey=0d08dc7787d24e62a5861bdb7c5842b8'
     fetch(url)
     .then(res=>res.json())
-    .then(data => setArticles(data.articles))
+    .then(data => console.log(data.articles))
+  },[])
+  useEffect(()=>{
+    const url='https://newsapi.org/v2/top-headlines?country=us&apiKey=0d08dc7787d24e62a5861bdb7c5842b8'
+    axios(url) 
+    .then(data => setArticles(data.data.articles))
   },[])
   return (
     <div>
